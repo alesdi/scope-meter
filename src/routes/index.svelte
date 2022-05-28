c3a7f19236869bd336610e154a4d66edcc29befe
Alessandro Diano <alessandro.diano@me.com>
2022-05-28 18:43:27 +0200

    Update layout and style

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,20 +7,39 @@
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>ScopeMeter</title>
+	<meta
+		name="description"
+		content="Quick and intuitive metering and analysis tool for oscilloscope captured screenshots"
+	/>
 </svelte:head>
 
 <section>
+	<!--Logo -->
+	<div class="logo">
+		<a href="/">
+			<img src="/logo.svg" alt="ScopeMeter" />
+		</a>
+	</div>
 	<ScopeBox />
 </section>
 
 <style>
+	.logo {
+		height: 2em;
+		margin-bottom: 1rem;
+		text-align: left;
+	}
+
+	.logo img {
+		height: 100%;
+	}
+
 	section {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
-		align-items: center;
+		align-items: left;
 		flex: 1;
 	}
 </style>
